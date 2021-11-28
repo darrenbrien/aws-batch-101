00abd317a7d3f8100df4d8c6a9f7986f641e46f9
Darren Brien <darrenbrien@aol.com>
2021-11-28 15:19:55 +0000

    Add fsx to deployment

--- aws_batch_101/aws_batch_101_stack.py
+++ aws_batch_101/aws_batch_101_stack.py
@@ -1,5 +1,6 @@
 import os
 from aws_cdk import (
+    aws_fsx as fsx,
     aws_iam as iam,
     aws_s3_assets as s3_assets,
     aws_s3 as s3,
@@ -30,7 +31,16 @@ class AwsBatch101Stack(core.Stack):
             path=os.path.join(cwd, "docker", "script")
         )
         bucket = s3.Bucket(self, id="batch")
-        vpc = ec2.Vpc(self, "vpc", max_azs=3)
+        vpc = ec2.Vpc(
+            self,
+            "vpc",
+            gateway_endpoints={
+                "S3": ec2.GatewayVpcEndpointOptions(
+                    service=ec2.GatewayVpcEndpointAwsService.S3
+                )
+            },
+            max_azs=3,
+        )
 
         sg = ec2.SecurityGroup(self, "sg", vpc=vpc)
 
@@ -74,6 +84,39 @@ class AwsBatch101Stack(core.Stack):
 
         batchServiceRole.add_to_policy(stsAssumeRoleStatement)
 
+        fsx_security_group = ec2.SecurityGroup(
+            self,
+            "FsxSg",
+            vpc=vpc,
+            allow_all_outbound=True,
+            security_group_name="fsx-lustre-sg",
+        )
+
+        fsx_security_group.add_ingress_rule(
+            peer=ec2.Peer.ipv4(vpc.vpc_cidr_block),
+            connection=ec2.Port.tcp(988),
+            description="FSx Lustre",
+        )
+
+        fsx_filesystem = fsx.CfnFileSystem(
+            self,
+            "Fsx",
+            file_system_type="LUSTRE",
+            storage_capacity=1200,
+            subnet_ids=[
+                vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE).subnet_ids[0]
+            ],
+            security_group_ids=[fsx_security_group.security_group_id],
+            tags=[core.CfnTag(key="Name", value="fsx-lustre")],
+            lustre_configuration=fsx.CfnFileSystem.LustreConfigurationProperty(
+                auto_import_policy="NONE",
+                deployment_type="SCRATCH_2",
+                data_compression_type="LZ4",
+                import_path=f"s3://{bucket.bucket_name}/",
+                export_path=f"s3://{bucket.bucket_name}/fsx",
+            ),
+        )
+
         instanceRole = iam.Role(
             self, "instance-role",
             assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
@@ -82,6 +125,30 @@ class AwsBatch101Stack(core.Stack):
         )
         instanceRole.add_to_policy(stsAssumeRoleStatement)
         instanceRole.add_to_policy(s3RoleStatement)
+
+        fsx_directory = '/fsx'
+        fsx_user_data = f"""MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
+
+--==MYBOUNDARY==
+Content-Type: text/cloud-config; charset="us-ascii"
+
+runcmd:
+- fsx_directory=/fsx
+- amazon-linux-extras install -y lustre2.10
+- mkdir -p ${{fsx_directory}}
+- mount -t lustre {fsx_filesystem.attr_dns_name}@tcp:/{fsx_filesystem.attr_lustre_mount_name} ${{fsx_directory}}
+
+--==MYBOUNDARY==--
+"""
+        fsx_lt = ec2.CfnLaunchTemplate(
+            self,
+            "FsxLT",
+            launch_template_data=ec2.CfnLaunchTemplate.LaunchTemplateDataProperty(
+                user_data=core.Fn.base64(fsx_user_data)
+            )
+        )
+        print(core.Fn.base64(fsx_user_data))
 
         asset = DockerImageAsset(self,
                                  "MyBuildImage",
@@ -106,7 +173,18 @@ class AwsBatch101Stack(core.Stack):
                     name="BATCH_FILE_S3_URL", value=script_asset.s3_object_url)
             ],
             image=asset.image_uri,
-            resource_requirements=resource_requirement
+            resource_requirements=resource_requirement,
+            mount_points=[batch.CfnJobDefinition.MountPointsProperty(
+                container_path=fsx_directory,
+                read_only=False,
+                source_volume="fsx"
+            )],
+            volumes=[
+                batch.CfnJobDefinition.VolumesProperty(
+                    name="fsx",
+                    host=batch.CfnJobDefinition.VolumesHostProperty(source_path=fsx_directory)
+                )
+            ]
         )
 
         jobDefinition = batch.CfnJobDefinition(
@@ -132,6 +210,10 @@ class AwsBatch101Stack(core.Stack):
             instance_types=[
                 "optimal"
             ],
+            launch_template=batch.CfnComputeEnvironment.LaunchTemplateSpecificationProperty(
+                launch_template_id=fsx_lt.ref,
+                version="$Latest"
+            ),
             instance_role=instanceProfile.attr_arn,
             spot_iam_fleet_role=spotServiceRole.role_arn,
             type="SPOT",
@@ -148,6 +230,7 @@ class AwsBatch101Stack(core.Stack):
             state="ENABLED"
         )
         computeEnvironment.add_depends_on(instanceProfile)
+        computeEnvironment.add_depends_on(fsx_lt)
 
         jobQueue = batch.CfnJobQueue(
             self,
@@ -179,12 +262,19 @@ my_config = Config(region_name=region)
 client = boto3.client('batch', config=my_config)
 
 def lambda_handler(event, context):
+    if "direction" in event:
+        direction = event["direction"]
+    else:
+        direction = "upload"
     job_name = f"{dt.datetime.now().strftime('%Y%m%d-%H%M%s')}-{uuid.uuid4()}"
     response = client.submit_job(
                         jobName=job_name,
                         jobQueue=os.environ['JOB_QUEUE'],
                         jobDefinition=os.environ['JOB_DEFINITION'],
-                        parameters={"key":job_name}
+                        parameters={
+                            "key":job_name,
+                            "direction" : direction
+                        }
     )
     print(response)
 '''),
@@ -216,20 +306,24 @@ from botocore.config import Config
 region = os.environ['AWS_REGION']
 my_config = Config(region_name=region)
 client = boto3.client('batch', config=my_config)
-s3 = boto3.resource("s3")
+s3 = boto3.resource("s3", config=my_config)
 bucket = s3.Bucket(os.environ['BUCKET_NAME'])
 
 def lambda_handler(event, context):
+    if "direction" in event:
+        direction = event["direction"]
+    else:
+        direction = "download"
     for key in bucket.objects.all():
-        job_name = f"download-{dt.datetime.now().strftime('%Y%m%d-%H%M%s')}-{key}"
+        job_name = f"download-{dt.datetime.now().strftime('%Y%m%d-%H%M%s')}-{key.key}"
         response = client.submit_job(
                             jobName=job_name,
                             jobQueue=os.environ['JOB_QUEUE'],
                             jobDefinition=os.environ['JOB_DEFINITION'],
-                                            parameters={
-                                                "key":, key,
-                                                "direction" : "download
-                                           }
+                            parameters={
+                                "key": key.key,
+                                "direction" : direction
+                            }
         )
         print(response)
 '''),
@@ -241,5 +335,5 @@ def lambda_handler(event, context):
                 "JOB_DEFINITION": jobDefinitionName,
                 "JOB_QUEUE": jobQueueName
             },
-            initial_policy=[jobSubmitStatement]
+            initial_policy=[jobSubmitStatement, s3RoleStatement]
         )
--- docker/script/main.py
+++ docker/script/main.py
@@ -1,32 +1,60 @@
 #!/usr/bin/env python
 import time
+import random
 import logging
-import jax.numpy as jnp
 from io import BytesIO
-import jax.random as random
+from hashlib import sha256
 import boto3
 import sys
 
 
+class CallbackStream(object):
+    """Wraps a file-like object in another, but also calls a user
+    callback with the number of bytes read whenever its `read()` method
+    is called. Used for tracking upload progress, for example for a
+    progress bar in a UI application. Idea taken from ActiveState Code Recipe:
+    http://code.activestate.com/recipes/578669-wrap-a-string-in-a-file-like-object-that-calls-a-u/
+    """
+
+    def __init__(self, file_like, callback):
+        self.file_like = file_like
+        self._callback = callback
+
+    def __len__(self):
+        raise NotImplementedError()
+
+    def read(self, *args):
+        chunk = self.file_like.read(*args)
+        if len(chunk) > 0:
+            self._callback(len(chunk))
+        return chunk
+
+    def write(self, bytes_, *args):
+        chunk = self.file_like.write(bytes_, *args)
+        if chunk > 0:
+            self._callback(chunk)
+        return chunk
+
+
 class Throughput:
-    def __init__(self, format_string):
+    def __init__(self, direction):
         self._ttl_bytes_ = 0
         self._start_time_ = time.perf_counter()
         self._bytes_increment_ = 2**28
-        self._format_string_ = "{ttl_bytes / 2**20:.1f}MiB " + format_string
+        self._direction_ = direction
 
     def transfer(self, b):
-        self.ttl_bytes += b
-        if b % self._bytes_increment_ == 0:
-            logging.info(self._format_string_.format(ttl_bytes=self._ttl_bytes_))
+        self._ttl_bytes_ += b
+        if self._ttl_bytes_ % self._bytes_increment_ == 0:
+            logging.info(f"{self._ttl_bytes_ / 2 ** 20:.1f}MiB {self._direction_}")
 
-    def _enter_(self):
+    def __enter__(self):
         return self
 
-    def _exit_(self, exec_type, exec_value, traceback):
+    def __exit__(self, exec_type, exec_value, traceback):
         elapsed = time.perf_counter() - self._start_time_
         ttl_mib = self._ttl_bytes_ / 2**20
-        logging.info(f"{ttl_mib}MiB in {elapsed:.1f} secs, {ttl_mib/elapsed:.1f}MiB/Sec")
+        logging.info(f"{ttl_mib:.1f}MiB in {elapsed:.1f} secs, {ttl_mib/elapsed:.1f}MiB/Sec")
 
 
 def download(client, bucket, key):
@@ -38,20 +66,25 @@ def download(client, bucket, key):
             Key=key,
             Callback=thr.transfer
         )
-    data = jnp.load(bytes_)
-    logging.info(f"Array of size {data.shape}")
+    h1 = sha256()
+    h1.update(bytes_.read())
+    aux = h1.digest()
+    logging.info(f"SHA256 {aux}")
+
+
+def get_data(num_bytes):
+    blocks = 2**16
+    ttl_blocks = int(num_bytes // blocks)
+    bytes_ = BytesIO(
+        bytes.join(
+            b'',
+            [random.getrandbits(8 * blocks).to_bytes(blocks, sys.byteorder)] * ttl_blocks)
+    )
+    return bytes_
 
 
 def upload(client, bucket, key):
-    seed = 19590414
-    rng_key = random.PRNGKey(seed)
-    columns = 1000
-    rows = 500000
-    data = random.normal(key=rng_key, shape=(rows, columns))
-    bytes_ = BytesIO()
-    jnp.save(bytes_, data, allow_pickle=False)
-    bytes_.seek(0)
-
+    bytes_ = get_data(3.5 * 1024**3)
     with Throughput(f"uploaded to s3://{bucket}/{key}") as thr:
         client.upload_fileobj(
             Fileobj=bytes_,
@@ -61,14 +94,36 @@ def upload(client, bucket, key):
         )
 
 
+def read(key):
+    with open(f'/fsx/{key}', mode='rb') as file, Throughput(f"read from fsx://{key}") as thr:
+        h1 = sha256()
+        cs = CallbackStream(file, thr.transfer)
+        data = cs.read()
+    h1.update(data)
+    aux = h1.digest()
+    logging.info(f"SHA256 {aux}")
+
+
+def write(key):
+    length = int(3.5 * 1024**3)
+    bytes_ = get_data(length)
+    with open(f'/fsx/{key}', mode='wb') as file, Throughput(f"write to fsx://{key}") as thr:
+        cs = CallbackStream(file, thr.transfer)
+        cs.write(bytes_.read())
+
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s %(message)s')
     try:
-        _, bucket, key, *rest = sys.argv
+        _, bucket, key, rest, *_ = sys.argv
         client = boto3.client("s3")
-        if rest and rest[0] == "download":
+        if rest == "download":
             download(client, bucket, key)
-        else:
+        elif rest == "upload":
             upload(client, bucket, key)
+        elif rest == "fsx-up":
+            write(key)
+        else:
+            read(key)
     except Exception:
         logging.exception("Failed to transfer data to/from bucket")
